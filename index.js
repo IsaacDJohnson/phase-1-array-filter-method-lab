2b1a27ffa491dba4ee4ee26831f62cf4a5eab219
IsaacDJohnson <isaac.designws@gmail.com>
2023-05-10 10:46:13 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(array, string){
+    let newArray = []
+    for (const name of array){
+        if (name === string || name === string.toLowerCase()){
+            newArray.push(name)
+        }
+    }
+    return newArray
+}
+
+function fuzzyMatch(array, string){
+    let newArray = []
+    for (const name of array){
+        if (name[0] === string[0]){
+            newArray.push(name)
+        }
+    }
+    return newArray;
+}
+
+function matchName(array, string){
+    let newArray = []
+    for (const name of array){
+        if (name.name === string){
+        newArray.push(name)
+        }
+    }
+    return newArray
+}
